79c4349fc241bef7046c397609e8248ae72ab3e9
Jlesliek24 <joshua.leslie@student.umn.ac.id>
2025-02-07 10:02:13 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Joshua Leslie Arihadi - 00000075451</Text>
       <StatusBar style="auto" />
     </View>
   );
